5cb5ebfcbd56f4a12db7f7c174bba98c25d82c33
DevExpressExampleBot <fakeEmail>
2022-07-26 10:51:55 +0400

    Source auto update [skip ci]

--- VB/NewItemRow_CodeBehind/MainWindow.xaml.vb
+++ VB/NewItemRow_CodeBehind/MainWindow.xaml.vb
@@ -1,55 +1,61 @@
-﻿Imports DevExpress.Xpf.Grid
+Imports DevExpress.Xpf.Grid
 Imports System.Collections.Generic
-Imports System.Collections.ObjectModel
 Imports System.Linq
 Imports System.Windows
 
 Namespace NewItemRow_CodeBehind
-	Public Class Product
-		Public Property ProductName() As String
-		Public Property CompanyName() As String
-		Public Property UnitPrice() As Integer
-		Public Property Discontinued() As Boolean
 
-		Public Sub New()
-		End Sub
-		Public Sub New(ByVal i As Integer)
-			ProductName = "ProductName" & i
-			CompanyName = "CompanyName" & i
-			UnitPrice = i
-			Discontinued = i Mod 2 = 0
-		End Sub
-	End Class
+    Public Class Product
 
-	Partial Public Class MainWindow
-		Inherits Window
+        Public Property ProductName As String
 
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = GetProductList().ToList()
-		End Sub
-		Private Shared Function GetProductList() As IEnumerable(Of Product)
-			Return Enumerable.Range(0, 3).Select(Function(i) New Product(i))
-		End Function
+        Public Property CompanyName As String
 
-		Private Sub OnInitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
-			grid.SetCellValue(e.RowHandle, "UnitPrice", 10)
-			grid.SetCellValue(e.RowHandle, "CompanyName", "newcompany")
-			grid.SetCellValue(e.RowHandle, "Discontinued", False)
-		End Sub
+        Public Property UnitPrice As Integer
 
-		Private Sub OnValidateRow(ByVal sender As Object, ByVal e As GridRowValidationEventArgs)
-			If e.RowHandle = GridControl.NewItemRowHandle Then
-				e.IsValid = Not String.IsNullOrEmpty(DirectCast(e.Row, Product).ProductName)
-				e.Handled = True
-			End If
-		End Sub
+        Public Property Discontinued As Boolean
 
-		Private Sub OnInvalidRowException(ByVal sender As Object, ByVal e As InvalidRowExceptionEventArgs)
-			If e.RowHandle = GridControl.NewItemRowHandle Then
-				e.ErrorText = "Please enter the Product Name."
-				e.WindowCaption = "Input Error"
-			End If
-		End Sub
-	End Class
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal i As Integer)
+            ProductName = "ProductName" & i
+            CompanyName = "CompanyName" & i
+            UnitPrice = i
+            Discontinued = i Mod 2 = 0
+        End Sub
+    End Class
+
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = GetProductList().ToList()
+        End Sub
+
+        Private Shared Function GetProductList() As IEnumerable(Of Product)
+            Return Enumerable.Range(0, 3).[Select](Function(i) New Product(i))
+        End Function
+
+        Private Sub OnInitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+            Me.grid.SetCellValue(e.RowHandle, "UnitPrice", 10)
+            Me.grid.SetCellValue(e.RowHandle, "CompanyName", "newcompany")
+            Me.grid.SetCellValue(e.RowHandle, "Discontinued", False)
+        End Sub
+
+        Private Sub OnValidateRow(ByVal sender As Object, ByVal e As GridRowValidationEventArgs)
+            If e.RowHandle = DataControlBase.NewItemRowHandle Then
+                e.IsValid = Not String.IsNullOrEmpty(CType(e.Row, Product).ProductName)
+                e.Handled = True
+            End If
+        End Sub
+
+        Private Sub OnInvalidRowException(ByVal sender As Object, ByVal e As InvalidRowExceptionEventArgs)
+            If e.RowHandle = DataControlBase.NewItemRowHandle Then
+                e.ErrorText = "Please enter the Product Name."
+                e.WindowCaption = "Input Error"
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/NewItemRow_MVVM/MainViewModel.vb
+++ VB/NewItemRow_MVVM/MainViewModel.vb
@@ -1,58 +1,67 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.Xpf
-Imports System
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Linq
 
 Namespace NewItemRow_MVVM
-	Public Class Product
-		Public Property ProductName() As String
-		Public Property CompanyName() As String
-		Public Property UnitPrice() As Integer
-		Public Property Discontinued() As Boolean
 
-		Public Sub New()
-		End Sub
-		Public Sub New(ByVal i As Integer)
-			ProductName = "ProductName" & i
-			CompanyName = "CompanyName" & i
-			UnitPrice = i
-			Discontinued = i Mod 2 = 0
-		End Sub
-	End Class
+    Public Class Product
 
-	Public Class MainViewModel
-		Inherits ViewModelBase
+        Public Property ProductName As String
 
-		Public ReadOnly Property ProductList() As ObservableCollection(Of Product)
-		Public Sub New()
-			ProductList = New ObservableCollection(Of Product)(GetProductList())
-		End Sub
-		Private Shared Function GetProductList() As IEnumerable(Of Product)
-			Return Enumerable.Range(0, 3).Select(Function(i) New Product(i))
-		End Function
+        Public Property CompanyName As String
 
-		<Command>
-		Public Sub InitNewRow(ByVal args As InitNewRowArgs)
-			Dim product = DirectCast(args.Item, Product)
-			product.UnitPrice = 10
-			product.CompanyName = "newcompany"
-			product.Discontinued = False
-		End Sub
-		<Command>
-		Public Sub ValidateRow(ByVal args As RowValidationArgs)
-			If args.IsNewItem AndAlso String.IsNullOrEmpty(DirectCast(args.Item, Product).ProductName) Then
-				args.Result = New ValidationErrorInfo("Please enter the Product Name.")
-			End If
-		End Sub
-		<Command>
-		Public Sub InvalidRow(ByVal args As InvalidRowExceptionArgs)
-			If args.IsNewItem Then
-				args.ErrorText = "Please enter the Product Name."
-				args.WindowCaption = "Input Error"
-			End If
-		End Sub
-	End Class
+        Public Property UnitPrice As Integer
+
+        Public Property Discontinued As Boolean
+
+        Public Sub New()
+        End Sub
+
+        Public Sub New(ByVal i As Integer)
+            ProductName = "ProductName" & i
+            CompanyName = "CompanyName" & i
+            UnitPrice = i
+            Discontinued = i Mod 2 = 0
+        End Sub
+    End Class
+
+    Public Class MainViewModel
+        Inherits ViewModelBase
+
+        Public ReadOnly Property ProductList As ObservableCollection(Of Product)
+
+        Public Sub New()
+            ProductList = New ObservableCollection(Of Product)(GetProductList())
+        End Sub
+
+        Private Shared Function GetProductList() As IEnumerable(Of Product)
+            Return Enumerable.Range(0, 3).[Select](Function(i) New Product(i))
+        End Function
+
+        <Command>
+        Public Sub InitNewRow(ByVal args As InitNewRowArgs)
+            Dim product = CType(args.Item, Product)
+            product.UnitPrice = 10
+            product.CompanyName = "newcompany"
+            product.Discontinued = False
+        End Sub
+
+        <Command>
+        Public Sub ValidateRow(ByVal args As RowValidationArgs)
+            If args.IsNewItem AndAlso String.IsNullOrEmpty(CType(args.Item, Product).ProductName) Then
+                args.Result = New ValidationErrorInfo("Please enter the Product Name.")
+            End If
+        End Sub
+
+        <Command>
+        Public Sub InvalidRow(ByVal args As InvalidRowExceptionArgs)
+            If args.IsNewItem Then
+                args.ErrorText = "Please enter the Product Name."
+                args.WindowCaption = "Input Error"
+            End If
+        End Sub
+    End Class
 End Namespace
