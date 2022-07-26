8d7e951733cc79410421dcf65f46036c47d98f38
DevExpressExampleBot <fakeEmail>
2022-07-26 10:48:04 +0400

    Source auto update [skip ci]

--- VB/DXGrid_NewItemRow/Window1.xaml.vb
+++ VB/DXGrid_NewItemRow/Window1.xaml.vb
@@ -1,49 +1,35 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports DevExpress.Xpf.Grid
 Imports System.Data
 
 Namespace DXGrid_NewItemRow
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid.ItemsSource = New nwindDataSetTableAdapters.ProductsTableAdapter().GetData().DefaultView
-		End Sub
 
-		Private Sub TableView_InitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
-			grid.SetCellValue(e.RowHandle, "UnitPrice", 10)
-			grid.SetCellValue(e.RowHandle, "Discontinued", False)
-			grid.SetCellValue(e.RowHandle, "UnitsOnOrder", 1)
-		End Sub
+    Public Partial Class Window1
+        Inherits Window
 
-		Private Sub TableView_ValidateRow(ByVal sender As Object, ByVal e As GridRowValidationEventArgs)
-			If e.Row Is Nothing Then
-				Return
-			End If
-			If e.RowHandle = GridControl.NewItemRowHandle Then
-				e.IsValid = (CType(e.Row, DataRowView))("ProductName").ToString() <> String.Empty
-			End If
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = New nwindDataSetTableAdapters.ProductsTableAdapter().GetData().DefaultView
+        End Sub
 
-		Private Sub TableView_InvalidRowException(ByVal sender As Object, ByVal e As InvalidRowExceptionEventArgs)
-			If e.RowHandle = GridControl.NewItemRowHandle Then
-				e.ErrorText = "Please enter the Product name. "
-				e.WindowCaption = "Input Error"
-                grid.CurrentColumn = grid.Columns("ProductName")
-			End If
-		End Sub
-	End Class
+        Private Sub TableView_InitNewRow(ByVal sender As Object, ByVal e As InitNewRowEventArgs)
+            Me.grid.SetCellValue(e.RowHandle, "UnitPrice", 10)
+            Me.grid.SetCellValue(e.RowHandle, "Discontinued", False)
+            Me.grid.SetCellValue(e.RowHandle, "UnitsOnOrder", 1)
+        End Sub
+
+        Private Sub TableView_ValidateRow(ByVal sender As Object, ByVal e As GridRowValidationEventArgs)
+            If e.Row Is Nothing Then Return
+            If e.RowHandle = DataControlBase.NewItemRowHandle Then e.IsValid = Not Equals(CType(e.Row, DataRowView)("ProductName").ToString(), String.Empty)
+        End Sub
+
+        Private Sub TableView_InvalidRowException(ByVal sender As Object, ByVal e As InvalidRowExceptionEventArgs)
+            If e.RowHandle = DataControlBase.NewItemRowHandle Then
+                e.ErrorText = "Please enter the Product name. "
+                e.WindowCaption = "Input Error"
+                Me.grid.CurrentColumn = Me.grid.Columns("ProductName")
+            End If
+        End Sub
+    End Class
 End Namespace
